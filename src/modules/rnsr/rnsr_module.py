9c532d83c8738a7a6d637c903166fd204248cbda
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2023-03-31 08:53:21 +0200

    Search by title in RNSR

--- src/modules/rnsr/rnsr_module.py
+++ src/modules/rnsr/rnsr_module.py
@@ -5,11 +5,13 @@ from typing import Iterator
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from bleach.sanitizer import Cleaner
 
-from app.module_mgmt.exceptions import EntityNotFoundError
+from app.module_mgmt.exceptions import EntityNotFoundError, DuplicateEntitiesError
 from app.module_mgmt.module import Module
 
 
@@ -32,13 +34,15 @@ class RnsrModule(Module):
         entity = super().entity(entity_type=entity_type, field=field, value=value)
         acronym = value if field == 'acronym' else None
         rnsr_code = value if field == 'code' else None
-        data = self._get_research_structure_data(rnsr_code=rnsr_code, acronym=acronym)
+        title = value if field == 'title' else None
+        number = value if field == 'number' else None
+        data = self._get_research_structure_data(rnsr_code=rnsr_code, acronym=acronym, title=title, number=number)
         for key in data:
             setattr(entity, key, data[key])
         return entity
 
-    def _get_research_structure_data(self, rnsr_code=None, acronym=None):
-        result_page = self._get_entity_result_page(acronym, rnsr_code)
+    def _get_research_structure_data(self, rnsr_code=None, acronym=None, title=None, number=None):
+        result_page = self._get_entity_result_page(acronym, rnsr_code, title, number)
         acronym, number, title = self._extract_title_and_identifiers(acronym, result_page)
         director, director_email = self._extract_directors(result_page)
         address = self._extract_address(result_page)
@@ -49,10 +53,10 @@ class RnsrModule(Module):
                 'rnsr_id': number,
                 'address': address}
 
-    def _get_entity_result_page(self, acronym, rnsr_code):
+    def _get_entity_result_page(self, acronym, rnsr_code, title, number):
         driver = self._get_web_driver()
         driver.get(self.config['search_form_url'])
-        self._execute_query(driver, acronym, rnsr_code)
+        self._execute_query(driver, acronym, rnsr_code, title, number)
         return driver
 
     def _extract_address(self, driver):
@@ -86,6 +90,11 @@ class RnsrModule(Module):
 
     def _extract_title_and_identifiers(self, acronym, driver):
         try:
+            results = driver.find_element(By.XPATH, '//p[@class="titre"]').text
+            raise DuplicateEntitiesError(f"Multiples réponses RNSR : {results}")
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
+        try:
             title = driver.find_element(By.XPATH, '//h2').text.split('\n')[0]
         except selenium.common.exceptions.NoSuchElementException as exc:
             raise EntityNotFoundError(f"Non trouvé RNSR : {acronym}") from exc
@@ -97,16 +106,37 @@ class RnsrModule(Module):
         title = None if match_name is None else match_name.group(2)
         return acronym, num, title
 
-    def _execute_query(self, driver, acronym, rnsr_id):
+    def _execute_query(self, driver, acronym, rnsr_id, title, number):
         if rnsr_id is not None:
             driver.find_element(By.XPATH, '//input[@name="NUM_NAT_STRUCT"]').send_keys(rnsr_id)
         elif acronym is not None:
             driver.find_element(By.XPATH, '//input[@name="STRUCT.LIBELLE"]').send_keys(acronym)
+        elif title is not None:
+            driver.find_element(By.XPATH, '//input[@name="STRUCT.LIBELLE"]').send_keys(title)
+        elif number is not None:
+            number = re.sub("[^0-9]", "", number)
+            driver.find_element(By.XPATH, '//input[@name="NUMERO"]').send_keys(number)
         button = driver.find_element(By.XPATH, '//input[@name="RECHERCHE"]')
         wait = WebDriverWait(driver, 10)
         wait.until(EC.visibility_of(button))
         time.sleep(0.5)
+        self._select_univ_filter(driver)
         driver.execute_script("arguments[0].click();", button)
+
+    def _select_univ_filter(self, driver, attempt=0):
+        etab_select = Select(driver.find_element(By.XPATH, '//select[@name="CLE_ETABLISSEMENT"]'))
+        etab_name = "Paris 1 - Université Panthéon-Sorbonne Paris 1"
+        all_options = etab_select.options
+        selected_options: list[WebElement] = list(filter(lambda o: o.text == etab_name, all_options))
+        assert attempt < 3 or len(selected_options) > 0, \
+            f"Il n'y a pas d'établissement nommé '{etab_name}' dans le filtre 'Établissement 1' du formulaire RNSR"
+        assert len(selected_options) < 2, \
+            f"Il y a plus d'un établissement nommé '{etab_name}' dans le filtre 'Établissement 1' du formulaire RNSR"
+        if len(selected_options) == 0:
+            time.sleep(0.5)
+            self._select_univ_filter(driver, attempt + 1)
+        else:
+            etab_select.select_by_value(selected_options[0].get_attribute("value"))
 
     def _get_web_driver(self):
         options = webdriver.ChromeOptions()
